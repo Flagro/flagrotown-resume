668f3e5f95400b837e858a6a5b8b777f3fab036a
flagro <antonpgrey@gmail.com>
2023-07-04 00:01:27 +0200

    initialized data

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -14,17 +14,17 @@ import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
-import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
-import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
-import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+// import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
+// import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
+// import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
+// import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
+// import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
+// import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+// import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
+// import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
+// import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
+// import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
+// import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
 import profilepic from '../images/profilepic.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
@@ -33,7 +33,7 @@ import {
   ContactType,
   Hero,
   HomepageMeta,
-  PortfolioItem,
+  // PortfolioItem,
   SkillGroup,
   Social,
   TestimonialSection,
@@ -44,7 +44,7 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
+  title: "Anton Potapov's Resume",
   description: "Example site built with Tim Baker's react resume template",
 };
 
@@ -62,25 +62,25 @@ export const SectionId = {
   Testimonials: 'testimonials',
 } as const;
 
-export type SectionId = typeof SectionId[keyof typeof SectionId];
+export type SectionId = (typeof SectionId)[keyof typeof SectionId];
 
 /**
  * Hero section
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Anton Potapov.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Montenegro based <strong className="text-stone-100">Backend Developer</strong> with a strong passion for{' '}
+        <strong className="text-stone-100">Data Analysis</strong> and{' '}
+        <strong className="text-stone-100">NLP tasks</strong>.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, you can catch me training in <strong className="text-stone-100">Podgorica</strong>,
+        plucking my <strong className="text-stone-100">guitar</strong>, or exploring beautiful{' '}
+        <strong className="text-stone-100">Mountains of Montenegro</strong>.
       </p>
     </>
   ),
@@ -104,16 +104,18 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `Passionate software developer and data analyst with a strong background in competitive programming and a Bachelor's degree in Computer Science. Skilled in C/C++, high-performance computing (HPC), data analysis, relational databases, and NLP tasks. Excited to tackle complex software development and data analysis challenges with a strong foundation in mathematics and a commitment to continuous learning.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Montenegro, Podgorica', Icon: MapIcon},
+    {label: 'Age', text: '22', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Russian', Icon: FlagIcon},
+    {label: 'Interests', text: 'Backend Development, Data Analysis', Icon: SparklesIcon},
+    {
+      label: 'Study',
+      text: 'Moscow State University, Bachelor in Applied Mathemathics and Computer Science',
+      Icon: AcademicCapIcon,
+    },
+    {label: 'Employment', text: 'Currently Self-employed', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -122,6 +124,40 @@ export const aboutData: About = {
  */
 export const skills: SkillGroup[] = [
   {
+    name: 'Backend development',
+    skills: [
+      {
+        name: 'C++',
+        level: 10,
+      },
+      {
+        name: 'Python',
+        level: 8,
+      },
+      {
+        name: 'C++ CUDA/OpenMP/MPI',
+        level: 9,
+      },
+    ],
+  },
+  {
+    name: 'LLMs',
+    skills: [
+      {
+        name: 'Huggingface transformers',
+        level: 8,
+      },
+      {
+        name: 'Langchain',
+        level: 5,
+      },
+      {
+        name: 'Vector databases',
+        level: 7,
+      },
+    ],
+  },
+  {
     name: 'Spoken languages',
     skills: [
       {
@@ -129,63 +165,29 @@ export const skills: SkillGroup[] = [
         level: 10,
       },
       {
-        name: 'French',
-        level: 4,
+        name: 'Russian',
+        level: 10,
       },
       {
-        name: 'Spanish',
+        name: 'Serbian',
         level: 3,
       },
     ],
   },
   {
-    name: 'Frontend development',
+    name: 'Data Analysis',
     skills: [
       {
-        name: 'React',
+        name: 'PostgreSQL',
         level: 9,
       },
       {
-        name: 'Typescript',
+        name: 'Python Pandas/Seaborn/Graphviz',
+        level: 10,
+      },
+      {
+        name: 'Python-Excel integration',
         level: 7,
-      },
-      {
-        name: 'GraphQL',
-        level: 6,
-      },
-    ],
-  },
-  {
-    name: 'Backend development',
-    skills: [
-      {
-        name: 'Node.js',
-        level: 8,
-      },
-      {
-        name: 'Rust',
-        level: 5,
-      },
-      {
-        name: 'Golang',
-        level: 4,
-      },
-    ],
-  },
-  {
-    name: 'Mobile development',
-    skills: [
-      {
-        name: 'React Native',
-        level: 9,
-      },
-      {
-        name: 'Flutter',
-        level: 4,
-      },
-      {
-        name: 'Swift',
-        level: 3,
       },
     ],
   },
@@ -194,90 +196,102 @@ export const skills: SkillGroup[] = [
 /**
  * Portfolio section
  */
-export const portfolioItems: PortfolioItem[] = [
-  {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage1,
-  },
-  {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage2,
-  },
-  {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage3,
-  },
-  {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage4,
-  },
-  {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage5,
-  },
-  {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage11,
-  },
-];
+// export const portfolioItems: PortfolioItem[] = [
+//   {
+//     title: 'Project title 1',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage1,
+//   },
+//   {
+//     title: 'Project title 2',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage2,
+//   },
+//   {
+//     title: 'Project title 3',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage3,
+//   },
+//   {
+//     title: 'Project title 4',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage4,
+//   },
+//   {
+//     title: 'Project title 5',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage5,
+//   },
+//   {
+//     title: 'Project title 6',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage6,
+//   },
+//   {
+//     title: 'Project title 7',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage7,
+//   },
+//   {
+//     title: 'Project title 8',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage8,
+//   },
+//   {
+//     title: 'Project title 9',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage9,
+//   },
+//   {
+//     title: 'Project title 10',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage10,
+//   },
+//   {
+//     title: 'Project title 11',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://timbaker.me',
+//     image: porfolioImage11,
+//   },
+// ];
 
 /**
  * Resume section -- TODO: Standardize resume contact format or offer MDX
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'September 2019 - June 2023',
+    location: 'Moscow State Univesity',
+    title: 'Bachelor\'s degree in Applied Mathematics and Computer Science',
+    content: <p>At Moscow State University's Faculty of Computational Mathematics and Cybernetics, I acquired a solid mathematical foundation and honed my programming skills in C/C++ and ASM. I gained hands-on experience in software development and delved into high-performance computing (HPC) and distributed systems, utilizing technologies like CUDA, OpenMP, and MPI. My university years were dedicated to pushing the boundaries of computational mathematics and computer science, equipping me with a strong skill set for software development and beyond.</p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'September 2016 - June 2019',
+    location: 'Moscow Institute of Physics and Technology',
+    title: 'Correspondence Physics and Technology School (CPTS) at MIPT',
+    content: <p>The invaluable education I received during my years at the Correspondence Physics and Technology School (CPTS) at MIPT played a pivotal role in shaping my university experience. It provided me with a strong foundation in both mathematics and physics, which proved to be instrumental throughout my academic journey.</p>,
+  },
+  {
+    date: 'September 2015 - June 2019',
+    location: 'Bauman Moscow State Technical University',
+    title: 'Physics and Mathematics Lyceum No. 1580 at Bauman Moscow State Technical University',
+    content: <p>During my time at my high and middle school, I acquired a strong mathematical foundation and nurtured my passion for IT, particularly in the field of competitive programming. Throughout those years, I actively participated in math and IT summer schools, which contributed to my continuous improvement. These efforts led to notable achievements, including victories and prize placements in competitions such as the "Moscow School Olympiad in Informatics" and the "ITMO Informatics Olympiad."</p>,
+  },
+  {
+    date: 'September 2018 - June 2022',
+    location: 'Summer HPC academy, LKSH, MIPT, FoxFord',
+    title: 'Active participation in off-site mathematical and computer science schools',
+    content: <p>Active participation in esteemed off-site mathematical and computer science schools has been a cornerstone of my IT journey, providing me with boundless motivation and inspiration. These include notable programs such as LKSH (summer IT school), MIPT Math Summer School, FoxFord School, and the Summer HPC Academy. Through these immersive experiences, I have continually expanded my knowledge, honed my skills, and fostered a deep passion for the world of technology.</p>,
   },
 ];
 
@@ -336,27 +350,27 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'Feel free to reach out to me using either the submit form or through any of the listed platforms. I look forward to hearing from you!',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'flagrogrey@gmail.com',
+      href: 'mailto:flagrogrey@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Podgorica, Montenegro',
+      href: 'https://www.google.com/maps/place/Podgorica,+Montenegro/@42.4320979,19.1734787,12z',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: '@flagrogrey',
+      href: 'https://www.instagram.com/flagrogrey/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'flagro',
+      href: 'https://github.com/flagro',
     },
   ],
 };
@@ -365,9 +379,9 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/tbakerx/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/flagro'},
+  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/22146212/anton-potapov'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/flagrogrey/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/flagrogrey/'},
+  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/flagrogrey'},
 ];
